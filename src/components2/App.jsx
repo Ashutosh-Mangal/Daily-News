9e7bed885c2ec763f34d1a58bf9298972f561301
Ashutosh Mangal <ashutoshmangal99@gmail.com>
2022-10-07 23:09:38 +0530

    Page size set to 6.

--- src/components2/App.jsx
+++ src/components2/App.jsx
@@ -7,7 +7,7 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
 const App = ()=> {
-  const pageSize = 5;
+  const pageSize = 6;
   const apiKey = process.env.REACT_APP_NEWS_API2
   const [progress, setProgress] = useState(0)
  
